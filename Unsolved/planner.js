3de593eae4397322a758bbeed5a1d2f8235ae215
NCoder <nipa.dedhia@gmail.com>
2020-04-05 17:08:33 -0500

    created textarea

--- Unsolved/planner.js
+++ Unsolved/planner.js
@@ -1,8 +1,24 @@
-// var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-
+// display date and time on header
 $(document).ready(function () {
   var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
   $("#currentDay").text(currentDate);
-  console.log(currentDate);
-  date();
+  // console.log(currentDate);
+  // date();
+
+  // variable taskArea created for textarea tag
+  var taskArea = $("<textarea>");
+
+  //adding class to textarea
+  taskArea.addClass("textarea present col-md-12");
+  taskArea.attr("name", "Test");
+
+  // variable saveBtn created for button tag
+  var saveBtnEl = $("<button>");
+
+  //adding classes to save button
+  saveBtnEl.addClass("saveBtn");
+  saveBtnEl.attr("label", "save");
+
+  //display task area
+  $("#task").append(taskArea, saveBtnEl);
 });
