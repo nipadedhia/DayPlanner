fbb6dcef612c05717c7b90a8e91ea195f052b95d
NCoder <nipa.dedhia@gmail.com>
2020-04-06 09:07:59 -0500

    added save buttons

--- Unsolved/planner.js
+++ Unsolved/planner.js
@@ -9,16 +9,33 @@ $(document).ready(function () {
   var taskArea = $("<textarea>");
 
   //adding class to textarea
-  taskArea.addClass("textarea present col-md-12");
-  taskArea.attr("name", "Test");
+  // taskArea.addClass("textarea present col-md-9");
+  // taskArea.attr("name", "Test");
 
   // variable saveBtn created for button tag
   var saveBtnEl = $("<button>");
 
   //adding classes to save button
-  saveBtnEl.addClass("saveBtn");
-  saveBtnEl.attr("label", "save");
+  // saveBtnEl.addClass("saveBtn");
+  // saveBtnEl.attr("label", "save");
 
   //display task area
-  $("#task").append(taskArea, saveBtnEl);
+
+  for (var i = 1; i < 14; i++) {
+    var timeDisplay = $("<p>");
+    timeDisplay.addClass("hour col-md-2");
+
+    var taskArea = $("<textarea>");
+    taskArea.addClass("textarea description present col-md-9");
+    taskArea.attr("name", "Test");
+
+    var saveBtnEl = $("<button>");
+    saveBtnEl.addClass("saveBtn saveBtn i:hover col-md-1");
+    saveBtnEl.attr("label", "save");
+
+    $("#task").append(timeDisplay, taskArea, saveBtnEl);
+  }
 });
+
+var timeDifference = m.startOf("day").format();
+console.log(timeDifference);
