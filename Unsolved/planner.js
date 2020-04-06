2882487db8e903d07b59a7b06ca9b6046aa27932
NCoder <nipa.dedhia@gmail.com>
2020-04-06 17:54:29 -0500

    displays time slots

--- Unsolved/planner.js
+++ Unsolved/planner.js
@@ -1,29 +1,20 @@
 // display date and time on header
 $(document).ready(function () {
-  var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+  var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm a");
   $("#currentDay").text(currentDate);
-  // console.log(currentDate);
 
   // variable taskArea created for textarea tag
   var taskArea = $("<textarea>");
 
-  //adding class to textarea
-  // taskArea.addClass("textarea present col-md-9");
-  // taskArea.attr("name", "Test");
-
-  // // variable saveBtn created for button tag
-  // var saveBtnEl = $("<button>");
-  // saveBtnEl.addClass("fa fa-save");
-
   //display time for task area
-  // var taskTime = moment("8 am", "h a").format("h, a");
-  // console.log(taskTime);
-  // timeDisplay.append(taskTime);
-  // var taskTime = moment.format("h, a");
+  var startTime = moment("8 am", "h a").format("h a");
 
+  //Displays time slots
   for (var i = 1; i < 14; i++) {
     var timeDisplay = $("<p>");
     timeDisplay.addClass("hour col-md-2");
+    timeDisplay.append(startTime);
+    startTime = moment(startTime, "h a").add(1, "hour").format("h a");
 
     var taskArea = $("<textarea>");
     taskArea.addClass("textarea row present col-md-9");
@@ -35,22 +26,31 @@ $(document).ready(function () {
 
     $("#task").append(timeDisplay, taskArea, saveBtnEl, "<br>");
 
-    // var toDoTask = {
-    //   list: "",
-    // };
-
-    var time = ["8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6", "7"];
+    var time = [
+      "8 am",
+      "9am",
+      "10am",
+      "11am",
+      "12pm",
+      "1pm",
+      "2pm",
+      "3pm",
+      "4pm",
+      "5pm",
+      "6pm",
+      "7pm",
+    ];
     updateTime();
 
     function updateTime() {
       var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-      for (var i = 0; i < time.length; i++) {
-        if (parseInt(time[i]) > currentDate) {
-          $("#" + time[i]).addClass("future");
-        } else if (parseInt(time[i]) < currentDate) {
-          $("#" + time[i]).addClass("past");
-        } else if (parseInt(time[i]) == currentDate) {
-          $("#" + time[i]).addClass("present");
+      for (var time = 0; time < time.length; i++) {
+        if (time > currentDate) {
+          $("#task").addClass("future");
+        } else if (time < currentDate) {
+          $("#task").addClass("past");
+        } else if (time == currentDate) {
+          $("#task").addClass("present");
         }
       }
     }
